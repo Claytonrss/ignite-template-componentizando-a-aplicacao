9bbc648ed356f7441e24ceb1c13e030399fc10d3
Clayton Rafael <clayton.rssouza@gmail.com>
2022-03-10 21:29:46 -0300

    refactor: split app.tsx in components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,22 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from '../@types/custom'
+import { MovieCard } from './MovieCard'
+
+interface ContentProps {
+  movies: MovieProps[]
+}
+
+export function Content({ movies }: ContentProps) {
+  return (
+    <div className="movies-list">
+      {movies.map(movie => (
+        <MovieCard
+          key={movie.imdbID}
+          title={movie.Title}
+          poster={movie.Poster}
+          runtime={movie.Runtime}
+          rating={movie.Ratings[0].Value}
+        />
+      ))}
+    </div>
+  )
 }
